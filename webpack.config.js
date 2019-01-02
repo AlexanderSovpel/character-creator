8ac452d3fd599ead894524bd79a5c8cf1aeb4fb1
Alex Sovpel <asovpel@elinext.com>
2019-01-02 18:11:19 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react']
+      }
+    }]
+  },
+
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+};
